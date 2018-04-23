0d3aa9a17d1f6e759504968ed5544aec74ecc793
Christopher Grandoit <christopher.grandoit@simplybusiness.com>
2018-04-23 16:06:35 -0400

    added group radio buttons, displays group selection and filters by group on landing page

--- crashers/app/routes.js
+++ crashers/app/routes.js
@@ -63,7 +63,8 @@ module.exports = function(app, passport, db, ObjectId) {
       url: req.body.url,
       time: req.body.time,
       date: req.body.date,
-      description: req.body.description
+      description: req.body.description,
+      group: req.body.optradio
     }, (err, result) => {
       if (err) return console.log(err)
       console.log('saved to database')
