b1a0955ce6a4986118c10ac040ec2174d1fc7d3c
Ashley Marie Laing <ashleym.laing@gmail.com>
2018-04-23 13:52:39 -0400

    made changes to the routes and landing page while working with angel to align with the database

--- crashers/app/routes.js
+++ crashers/app/routes.js
@@ -4,44 +4,44 @@ module.exports = function(app, passport, db, ObjectId) {
 
   // show the home page (will also have our login links)
   app.get('/', function(req, res) {
-    db.collection('message').find().toArray((err, result) => {
+    db.collection('crash').find().toArray((err, result) => {
       if (err) return console.log(err)
       res.render('index.ejs', {
-        message: result
+        crash: result
       })
     })
   });
 
   app.get('/about', function(req, res) {
-    db.collection('message').find().toArray((err, result) => {
+    db.collection('crash').find().toArray((err, result) => {
       if (err) return console.log(err)
       res.render('about.ejs', {
-        message: result
+        crash: result
       })
     })
   });
 
-  app.get('/posting/:id', function(req, res) {
-    uId = ObjectId(req.params.id)
-    db.collection('message').findOne({
-      "_id": uId
-    }, (err, result) => {
-      if (err) return console.log(err)
-      res.render('ad-post.ejs', {
-        message: result
-      })
-    })
-  });
+  // app.get('/posting/:id', function(req, res) {
+  //   uId = ObjectId(req.params.id)
+  //   db.collection('crash').findOne({
+  //     "_id": uId
+  //   }, (err, result) => {
+  //     if (err) return console.log(err)
+  //     res.render('ad-post.ejs', {
+  //       crash: result
+  //     })
+  //   })
+  // });
 
 
 
   // PROFILE SECTION =========================
   app.get('/profile', isLoggedIn, function(req, res) {
-    db.collection('message').find().toArray((err, result) => {
+    db.collection('crash').find().toArray((err, result) => {
       if (err) return console.log(err)
       res.render('profile.ejs', {
         user: req.user,
-        message: result
+        crash: result
       })
     })
   });
@@ -54,10 +54,10 @@ module.exports = function(app, passport, db, ObjectId) {
     res.redirect('/');
   });
 
-  // message board routes ===============================================================
+  // crash board routes ===============================================================
 
-  app.post('/message', (req, res) => {
-    db.collection('message').save({
+  app.post('/crash', (req, res) => {
+    db.collection('crash').save({
       adTitle: req.body.adTitle,
       zip: req.body.zip,
       textArea: req.body.textArea,
@@ -86,7 +86,7 @@ module.exports = function(app, passport, db, ObjectId) {
   // show the login form
   app.get('/login', function(req, res) {
     res.render('login.ejs', {
-      message: req.flash('loginMessage')
+      crash: req.flash('logincrash')
     });
   });
 
@@ -94,14 +94,14 @@ module.exports = function(app, passport, db, ObjectId) {
   app.post('/login', passport.authenticate('local-login', {
     successRedirect: '/profile', // redirect to the secure profile section
     failureRedirect: '/login', // redirect back to the signup page if there is an error
-    failureFlash: true // allow flash messages
+    failureFlash: true // allow flash crashs
   }));
 
   // SIGNUP =================================
   // show the signup form
   app.get('/signup', function(req, res) {
     res.render('signup.ejs', {
-      message: req.flash('signupMessage')
+      crash: req.flash('signupMessage')
     });
   });
 
@@ -109,7 +109,7 @@ module.exports = function(app, passport, db, ObjectId) {
   app.post('/signup', passport.authenticate('local-signup', {
     successRedirect: '/profile', // redirect to the secure profile section
     failureRedirect: '/signup', // redirect back to the signup page if there is an error
-    failureFlash: true // allow flash messages
+    failureFlash: true // allow flash crashs
   }));
 
   // =============================================================================
